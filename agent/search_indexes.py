fa81faa1e9869f0c878305a4c60d9ebeba6fb65b
Matt Mansour <slackbabbath@gmail.com>
2014-06-10 07:58:51 -0700

    yes

--- agent/search_indexes.py
+++ agent/search_indexes.py
@@ -1,15 +1,16 @@
+import datetime
 from haystack import indexes
-
 from models import Agent
+
 
 class AgentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title')
-    body = indexes.CharField(model_attr='body')
+    agent = indexes.CharField(model_attr='full_name')
+    license_issue_date = indexes.DateTimeField(model_attr='license_issue_date')
 
     def get_model(self):
         return Agent
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(timestamp__lte=timezone.now())
+        return self.get_model().objects.filter(license_exp_date__gte=datetime.datetime.now())
